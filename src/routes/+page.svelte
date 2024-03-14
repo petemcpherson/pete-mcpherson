989f69a33f97d1597f7e8cecd84dd604aeaf2f4e
Pete McPherson <pnm326@gmail.com>
2024-03-13 20:56:49 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<h1>Welcome to the FUTURE home of Pete McPherson!</h1>
+<p>
+    This site used to be on WordPress, but Pete's building out a new portfolio
+    using SvelteKit + Cloudflare Pages (just for fun, and the challenge ;)
+</p>
+<p>For now, you can find me at...</p>
+<ul>
+    <li>
+        <a
+            href="https://doyouevenblog.com/"
+            target="_blank"
+            rel="noreferrer noopener">Do You Even Blog</a
+        > 👉 My award-winning (really!) podcast, YouTube channel, blog, and newsletter.
+    </li>
+    <li>
+        <a
+            href="https://keebnews.com/"
+            target="_blank"
+            rel="noreferrer noopener">KeebNews</a
+        > 👉 Blog and YouTube channel about Mechanical Keyboards.
+    </li>
+    <li>
+        <a
+            href="https://betternotetaking.com/"
+            target="_blank"
+            rel="noreferrer noopener">BetterNoteTaking.com</a
+        > 👉 My brand-spankin-new brand all about Knowledge Management.
+    </li>
+</ul>
+<p>Also, here are some fun tools I'm building...</p>
+<ul>
+    <li>
+        <a
+            href="https://fabb.ai/"
+            target="_blank"
+            rel="noreferrer noopener">FABB.ai</a
+        > 🤖 AI blogging app.
+    </li>
+    <li>
+        <a
+            href="https://promptimizer.com/"
+            target="_blank"
+            rel="noreferrer noopener">Promptimizer</a
+        > 📝 Organize your fave prompts and copy them in 1 click.
+    </li>
+    <li>
+        <a
+            href="https://topicalmap.ai/"
+            target="_blank"
+            rel="noreferrer noopener">Topical Map AI</a
+        > 🌎 SEO keywords for a full site (organized for topical authority) in 1 click.
+    </li>
+</ul>
