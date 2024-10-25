1a88e53a2f5a55204645557f3f697628caffd429
Pete McPherson <pnm326@gmail.com>
2024-10-25 08:28:37 -0400

    starting tool page

---
+++ src/routes/tools/+page.js
@@ -0,0 +1,13 @@
+export async function load({  }) {
+    const post = {
+        title: "The tools I use for a hyper productive business (and life).",
+        description: "I'm a hyper-productive creator & entpreneur who runs 10+ businesses in about 20 hours a week. Here are the tools I live off of:",
+        author: "Pete McPherson",
+        updated: "2024-10-25",
+        tags: ['mac', 'tools', 'tech', 'productivity']
+    };
+
+    return {
+        post
+    };
+}
---
+++ src/routes/tools/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Head from '$lib/components/Head.svelte';
+	export let data;
+
+	const post = data.post;
+	const title = post.title;
+	const description = post.description;
+</script>
+
+<Head {title} {description} />
+
+<!-- start content -->
