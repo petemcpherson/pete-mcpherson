08c044e62151cfd691be459959f747513b5e2d75
Pete McPherson <pnm326@gmail.com>
2025-04-02 17:50:40 -0400

    installed storage

--- src/lib/server/admin.js
+++ src/lib/server/admin.js
@@ -1,5 +1,6 @@
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
+import { getStorage } from 'firebase-admin/storage';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID } from '$env/static/private';
 import pkg from 'firebase-admin';
 
@@ -9,7 +10,9 @@ try {
 			projectId: FB_PROJECT_ID,
 			clientEmail: FB_CLIENT_EMAIL,
 			privateKey: FB_PRIVATE_KEY
-		})
+		}),
+		storageBucket: 'pete-mcpherson-com.firebasestorage.app'
+		// gs://pete-mcpherson-com.firebasestorage.app
 	});
 } catch (e) {
 	if (!/already exists/u.test(e.message)) {
@@ -19,3 +22,4 @@ try {
 
 export const adminDB = getFirestore();
 export const adminAuth = getAuth();
+export const adminStorage = getStorage();
